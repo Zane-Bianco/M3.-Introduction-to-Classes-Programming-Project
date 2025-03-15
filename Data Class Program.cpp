3f012ef569aad4948a904194bbd4c8840b58a290
Zane-Bianco <zanebianco1@gmail.com>
2025-03-15 18:55:26 -0400

    Should be finished, just need to add comments.

--- Data Class Program.cpp
+++ Data Class Program.cpp
@@ -3,35 +3,48 @@
 
 #include <iostream>
 #include "Date.h"
-#include <string>
 using namespace std;
 
 int main()
 {
+    cout << "Test default constructor:" << "\n";
     Date date;
     date.printMonthDayYear();
     
+    cout << "Test constructor with valid date (2/28/2009):" << "\n";
     Date date1(2, 28, 2009);
     date1.printMonthDayYear();
     
+    cout << "Test constructor with invalid month (45/2/2009):" << "\n";
     Date date2(45, 2, 2009);
     date2.printMonthDayYear();
-
+    
+    cout << "Test constructor with invalid day (2/29/2009):" << "\n";
     Date date3(2, 29, 2009);
     date3.printMonthDayYear();
 
-    Date date4(13);
-    date4.printMonthDayYear();
+    cout << "Test setDate with bad month (13):" << "\n";
+    date.setDate(13,0,0);
+    date.printMonthDayYear();
 
-    Date date5(4, 31, 2009);
-    date5.printMonthDayYear();
+    cout << "Test setDate with bad day (4,31,2009):" << "\n";
+    date.setDate(4, 31, 2009);
+    date.printMonthDayYear();
 
+    cout << "Test for leap year with bad date (2/29/2009):" << "\n";
     Date date6(2, 29, 2009);
     date6.printMonthDayYear();
 
+    cout << "Test for leap year with good date (2/29/2008):" << "\n";
     Date date7(2, 29, 2008);
     date7.printMonthDayYear();
 
+    cout<< "Test constructor for invalid year (2/22/0):" << "\n";
+    Date date8(2, 22, 0);
+    date8.printMonthDayYear();
+
+    cout << "Test other print formats" << "\n";
     date7.printMonthNameDayYear();
     date7.printDayMonthNameYear();
+
 }
--- Date.h
+++ Date.h
@@ -2,26 +2,28 @@
 #ifndef date_h
 #define date_h
 #include <string>
-//Date class specification file - define members of the date class and the function defintions
 
+using namespace std;
+
+
+//Date class specification file - define members of the date class and the function defintions
 class Date {
 private:
 	int day, month, year;
 public:
-	void setDate(int d, int m, int y);
+	void setDate(int m, int d, int y);
 	inline int getDay() const { return day; };
 	inline int getMonth() const { return month; };
 	inline int getYear() const { return year; };
-	inline bool isLeapYear()const {return isLeapYear(year);};
+	bool isLeapYear()const;
 	bool isLeapYear(int year)const;
-	inline int lastDay()const { return lastDay(month, year); };
+	int lastDay()const;
 	int lastDay(int month, int year) const;
 	Date(int m = 1, int d = 1, int y = 1900);
 	string getMonthName(int month) const;
 	void printMonthDayYear() const; //// 12/25/2021
 	void printMonthNameDayYear() const; //// December 12, 2021
 	void printDayMonthNameYear() const; //// 12 December, 2021
-	
 };
 
 #endif
