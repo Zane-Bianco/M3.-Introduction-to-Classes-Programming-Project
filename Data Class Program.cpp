605b3d51dae59d8f21eb95cb88f57f23575964f3
Zane-Bianco <zanebianco1@gmail.com>
2025-03-15 00:05:30 -0400

    More Progress

--- Data Class Program.cpp
+++ Data Class Program.cpp
@@ -3,11 +3,35 @@
 
 #include <iostream>
 #include "Date.h"
-
+#include <string>
 using namespace std;
 
 int main()
 {
+    Date date;
+    date.printMonthDayYear();
+    
+    Date date1(2, 28, 2009);
+    date1.printMonthDayYear();
+    
+    Date date2(45, 2, 2009);
+    date2.printMonthDayYear();
 
-    std::cout << "Hello World!\n";
+    Date date3(2, 29, 2009);
+    date3.printMonthDayYear();
+
+    Date date4(13);
+    date4.printMonthDayYear();
+
+    Date date5(4, 31, 2009);
+    date5.printMonthDayYear();
+
+    Date date6(2, 29, 2009);
+    date6.printMonthDayYear();
+
+    Date date7(2, 29, 2008);
+    date7.printMonthDayYear();
+
+    date7.printMonthNameDayYear();
+    date7.printDayMonthNameYear();
 }
--- Date.cpp
+++ Date.cpp
@@ -1,51 +1,42 @@
 //Date.cpp - Date class implementation
 #include <iostream>
 #include "Date.h"
+#include <string>
 
+using namespace std;
 
 Date::Date(int m, int d, int y) {
 	setDate(m, d, y);
 }
 
 void Date::setDate(int m, int d, int y) {
-	
-	if (m < 1 || m > 12) {
-		std::cerr << "Month Invalid";
-			day = 1;
-			month = 1;
-			year = 1990;
-		if (y < 1) {
-			std::cerr << "Year Invalid\n";
-			day = 1;
-			month = 1;
-			year = 1990;
+	day = d;
+	month = m;
+	year = y;
 
-	}
-	}
-	if (d > lastDay() || d < 28) {
-		std::cerr << "Invalid day. Setting default date: 1/1/1990\n";
+	if (m < 1 || m > 12) {
+		cout << "Month Invalid\n";
 		day = 1;
 		month = 1;
-		year = 1990;
+		year = 1900;
 	}
-	else {
-		day = d;
-		month = m;
-		year = y;
+	if (y < 1) {
+		cout << "Year Invalid\n";
+		day = 1;
+		month = 1;
+		year = 1900;
+	}
+
+	if (d > lastDay() || d < 1) {
+		cout << "Day Invalid\n";
+		day = 1;
+		month = 1;
+		year = 1900;
 	}
 };
 
-int Date::lastDay() {
-	if (month == 4 || month == 6 || month == 9 || month == 11) {
-		return 30;
-	}
-	if (month == 2) {
-		return isLeapYear(year) ? 29 : 28;
-	}
-	return 31;
-};
 
-int Date::lastDay(int month, int year) {
+int Date::lastDay(int month, int year) const {
 	if (month == 4 || month == 6 || month == 9 || month == 11) {
 		return 30;
 	}
@@ -55,15 +46,25 @@ int Date::lastDay(int month, int year) {
 	return 31; 
 };
 
-int Date::getDay() const { return day; }
-int Date::getMonth() const { return month; }
-int Date::getYear() const { return year; }
-
-bool Date::isLeapYear() {
+bool Date::isLeapYear(int year) const {
 	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
-bool Date::isLeapYear(int year) {
-	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+void Date::printMonthDayYear() const {
+	cout << month << "/" << day << "/" << year <<"\n" <<"\n";
 }
 
+string Date::getMonthName(int month) const {
+	const string monthnames[12]{ "January", "February", "March", "April",
+		"May", "June", "July", "August",
+		"September", "October", "November", "December" };
+	return monthnames[month - 1];
+}
+
+void Date::printMonthNameDayYear() const {
+	cout << getMonthName(month) << " " << day << ", " << year;
+}
+
+void Date::printDayMonthNameYear() const {
+	cout << day << " " << getMonthName(month) << ", " << year;
+}
