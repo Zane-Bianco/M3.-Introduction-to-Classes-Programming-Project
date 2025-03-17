26226b6e92ad430f0be7c1ff09231c6c44173732
Zane-Bianco <zanebianco1@gmail.com>
2025-03-17 19:38:44 -0400

    Finito

--- Number Array Program.cpp
+++ Number Array Program.cpp
@@ -4,30 +4,62 @@
 
 #include <iostream>
 #include "numArray.h"
+#include <random>
 using namespace std;
 
 int main() {
 
-	numArray a(3);
-
-	a.setNumber(0, 2);
-
-	a.setNumber(1, 15);
-	a.setNumber(2, 13.1);
-	
-	a.getNumber(0);
-	
-	a.getNumber(1);
-	
-	a.getNumber(2);
-	
-	a.getMinimum();
-
-	a.getMaximum();
-
-	a.getAverage();
-
+	//Tests the default constructor
+	numArray a;
+	cout << "Test default constructor" << endl;
 	a.printArray();
+	cout << endl;
+	
+	//Tests constructor with parameters
+	numArray b(15);
+	cout << "Test constructor with parameter" << endl;
+	b.printArray();
+	cout << endl;
+
+	//populates b's array with random double values from 0 - 100
+	//then prints the array
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_real_distribution<double> dist(0.0, 100.0);
+	cout << "Array filled with random doubles" << endl;
+	for (int i = 0; i < b.getsize(); i++) {
+		b.setNumber(i, dist(gen));
+	}
+	b.printArray();	
+	cout << endl;
+	
+	//Test for out of bound index
+	cout << "Test for setting an invalid index" << endl;
+	b.setNumber(20, 2);
+	cout << endl;
+
+	//Test for retrieving value at index 5
+	cout << "Test for retrieving value at an index" << endl;
+	b.getNumber(5);
+	cout << endl;
+
+	//Test for retrieving out of bound index 
+	cout << "Test for retrieving invalid index" << endl;
+	b.getNumber(20);
+	cout << endl;
+
+	//Test for getting the minumum value within the array
+	b.getMinimum();
+	cout << endl;
+
+	//Test for getting the maximum value within the array
+	b.getMaximum();
+	cout << endl;
+
+	//Test for retrieving the average of the values within the array
+	b.getAverage();
+	
+	
 
 
 
--- numArray.cpp
+++ numArray.cpp
@@ -2,13 +2,17 @@
 
 #include <iostream>
 #include "numArray.h"
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-// Constructor with size = max_Size, max_Size defaulted to 10 
+// Constructor with size = max_Size, max_Size defaulted to 10
+// If size >= 20 or size < 1 size is set to te default size otherwise 
+// it makes a pointer to a double array of that size
 numArray::numArray(int size) {
 	cout << "Constructor: Memory Allocated" << endl;
-	if (size > 20 || size < 1) {
+	if (size >= 20 || size < 1) {
 		this ->size = max_Size;
 		cerr << "Array size invalid setting default." << endl;
 	}
@@ -20,28 +24,37 @@ numArray::numArray(int size) {
 
 
 //Function to assign a value to an index within the array (mutator)
+//Sets the specified index to the given value, verifies that the index is within the array
+//If index is out of bounds output error and don't store value otherwise set the index to value
 void numArray::setNumber(int index, double value) {
-	if (index > size || index < 0) {
-		cerr << "Invalid index";
+	if (index >= size || index < 0) {
+		cerr << "Invalid index, number not stored" << endl;
 
 	}
 	else {
-		arr[index] = value;
+		arr[index] = round(value*10.0)/10.0;
 	}
 }
 
 //Function to retrieve the value from the array (accessor)
+//Gets the value stored at index after validation by returning it (prints statement for testing)
+//If given index fails validation it outputs an error and returns the default value of 0.0
 double numArray::getNumber(int index) {
 	if (index >= size || index < 0) {
-		cerr << "Invalid index";
+		cerr << "Invalid index, returning default value: 0.0" << endl;
 			return 0.0;
 	}
 	else {
+		cout << "The value at index "<< index << " is: " << arr[index] << endl;
 		return arr[index];
 	}
 }
 
 //Function to retrieve the minimum value within the array (accessor)
+//Gets the minimum value within the array
+//Assigns first element of array to the min variable then compares it to the other elements
+//if it finds one that is smaller, it is assigned to the min variable 
+//finally returns min value (prints statement for testing)
 double numArray::getMinimum() {
 	double min = arr[0];
 	for (int i = 1; i < size; i++) {
@@ -49,11 +62,16 @@ double numArray::getMinimum() {
 				min = arr[i];	
 			}	
 		}
+	cout << "The smallest value in the array is: " << min << endl;
 	return min;	
 	}
 	
 
 //Function to retrieve the maximum value within the array (accessor)
+//Gets the maxiumum value within the array
+//Assigns first element of array to the max variable then compares it to the other elements
+//if it finds one that is larger, it is assigned to the min variable 
+//finally returns max value (prints statement for testing)
 double numArray::getMaximum() {
 	double max = arr[0];
 	for (int i = 1; i < size; i++) {
@@ -61,28 +79,37 @@ double numArray::getMaximum() {
 			max = arr[i];
 		}
 	}
+	cout << "The largest value in the array is: " << max << endl;
 	return max;
 }
 
 
 //Function to calculate the average value within the array (accessor)
+//Gets the average of the values within the array. sum variable is set to the first element
+//iterates through array adding each index's value to the sum variable 
+//returns the sum / the size of the array (prints statement for testing)
 double numArray::getAverage() {
 	double sum = arr[0];
 	for (int i = 1; i < size; i++) {
 		(sum += arr[i]);
 		}	
+	cout << "The average value in the array is: " << sum/size << endl;
 	return sum / size;
 }
 	
 
 //Function to output the values within the array (accessor)
+//Print the array by iterating through array one by one and outputting them with a set precision of 1 
+//this displays all of the values up to their first decimal place
 void numArray::printArray() {
 	for (int i = 0; i < size; i++) {
-		cout << "{" << arr[i] << "}";
+		cout << "{" << fixed << setprecision(1) << arr[i] << "}";
 	}
+	cout << endl;
 }
 // Destructor
+// Deallocates memory created for the array
 numArray::~numArray() {
 	delete[] arr;
-	cout << "\nDestructor: Memory Deallocated" << endl;
+	cout << "\nDestructor is running" << endl;
 }
