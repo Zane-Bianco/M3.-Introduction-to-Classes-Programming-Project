c7f9e1db2c4f9b451e1a87d16b7ac04866110fe1
Zane-Bianco <zanebianco1@gmail.com>
2025-03-17 19:50:51 -0400

    Finito

--- Date.cpp
+++ Date.cpp
@@ -5,10 +5,14 @@
 
 using namespace std;
 
+// Constructor with parameters of the month, day, year
+// Calls the setDate function for validation
 Date::Date(int m, int d, int y) {
 	setDate(m, d, y);
 }
 
+//Function to set date variables in class after validation. 
+//If validation fails sets a default date of 1/1/1900
 void Date::setDate(int m, int d, int y) {
 	bool isValid = true;
 	
@@ -40,6 +44,10 @@ void Date::setDate(int m, int d, int y) {
 	}
 };
 
+//Function to figure out the current object's last day of the month as an integer
+//Takes the month and returns the number of days within that month
+//If month is 2 also takes the year and calls the isLeapYear function
+//If isLeapYear returns true last day is 29, if false it is 28
 int Date::lastDay() const {
 	if (month == 4 || month == 6 || month == 9 || month == 11) {
 		return 30;
@@ -50,6 +58,10 @@ int Date::lastDay() const {
 	return 31;
 }
 
+//Function to figure out the last day of an object's month
+//Takes the month and returns the number of days within that month
+//If month is 2 also takes the year and calls the isLeapYear function
+//If isLeapYear returns true last day is 29, if false it is 28
 int Date::lastDay(int month, int year) const {
 	if (month == 4 || month == 6 || month == 9 || month == 11) {
 		return 30;
@@ -60,10 +72,12 @@ int Date::lastDay(int month, int year) const {
 	return 31; 
 }
 
+//Function to print the stored date in the following format of 1/2/2010
 void Date::printMonthDayYear() const {
 	cout << month << "/" << day << "/" << year <<"\n" <<"\n";
 }
 
+//Function to get the month name corressponding to the integer value
 string Date::getMonthName(int month) const {
 	const string monthnames[12]{ "January", "February", "March", "April",
 		"May", "June", "July", "August",
@@ -71,19 +85,24 @@ string Date::getMonthName(int month) const {
 	return monthnames[month - 1];
 }
 
+//Function to determine if the object's current state year is a leapyear or not
+//needed to figure out the number of days in the month of February
 bool Date::isLeapYear() const {
 	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
 
 }
+//Function to determine if an object's year is a leap year or not
 bool Date::isLeapYear(int year) const {
 	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
+//Function to print in date format: January 2, 1990
 void Date::printMonthNameDayYear() const {
 	cout << getMonthName(month) << " " << day << ", " << year << "\n";
 }
 
+//Function to print in date format: January, 2 1990
 void Date::printDayMonthNameYear() const {
 	cout << day << " " << getMonthName(month) << ", " << year << "\n";
 }
--- Date.h
+++ Date.h
@@ -12,15 +12,15 @@ private:
 	int day, month, year;
 public:
 	void setDate(int m, int d, int y);
-	inline int getDay() const { return day; };
-	inline int getMonth() const { return month; };
-	inline int getYear() const { return year; };
+	inline int getDay() const { return day; }; //inline function to quickly return the object's day
+	inline int getMonth() const { return month; };//inline function to quickly return the object's month
+	inline int getYear() const { return year; };//inline function to quickly return the object's year
 	bool isLeapYear()const;
 	bool isLeapYear(int year)const;
 	int lastDay()const;
 	int lastDay(int month, int year) const;
-	Date(int m = 1, int d = 1, int y = 1900);
-	string getMonthName(int month) const;
+	Date(int m = 1, int d = 1, int y = 1900); //constructor 
+	string getMonthName(int month) const; // function to get the month name for other display formats
 	void printMonthDayYear() const; //// 12/25/2021
 	void printMonthNameDayYear() const; //// December 12, 2021
 	void printDayMonthNameYear() const; //// 12 December, 2021
